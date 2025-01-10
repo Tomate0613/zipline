0435ea7c6a4fdcbb394e65c4a47af1c0f156b6a4
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-01-10 23:37:22 +0100

    Add CreateCraftsAdditionsCompat

---
+++ src/main/java/dev/doublekekse/zipline/compat/createaddition/CreateCraftsAdditionsCompat.java
@@ -0,0 +1,14 @@
+package dev.doublekekse.zipline.compat.createaddition;
+
+import dev.doublekekse.zipline.Cables;
+import net.minecraft.client.Minecraft;
+
+public class CreateCraftsAdditionsCompat {
+    public static void register() {
+        /*
+        Cables.registerProvider(((offsetPlayerPos, squaredRadius) -> {
+            //Minecraft.getInstance().level.blockEntiy
+        }));
+         */
+    }
+}
