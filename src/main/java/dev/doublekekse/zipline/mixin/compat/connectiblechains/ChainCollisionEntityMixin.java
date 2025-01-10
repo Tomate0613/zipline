d28d0b3ed740ef9112cc1cae81bcb1c586fa74f7
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-01-10 17:32:08 +0100

    Fix ChainCollisionEntity mixin

--- src/main/java/dev/doublekekse/zipline/mixin/compat/connectiblechains/ChainCollisionEntityMixin.java
+++ src/main/java/dev/doublekekse/zipline/mixin/compat/connectiblechains/ChainCollisionEntityMixin.java
@@ -2,23 +2,39 @@ package dev.doublekekse.zipline.mixin.compat.connectiblechains;
 
 import com.github.legoatoom.connectiblechains.entity.ChainCollisionEntity;
 import dev.doublekekse.zipline.registry.ZiplineItems;
-import net.minecraft.client.Minecraft;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ChainCollisionEntity.class)
-public class ChainCollisionEntityMixin {
-    @Inject(method = "canBeCollidedWith", at = @At("HEAD"), cancellable = true)
-    void canBeCollidedWith(CallbackInfoReturnable<Boolean> cir) {
-        assert Minecraft.getInstance().player != null;
+public abstract class ChainCollisionEntityMixin extends Entity {
+    @Shadow
+    public abstract InteractionResult interact(Player player, InteractionHand hand);
 
-        var mainHand = Minecraft.getInstance().player.getMainHandItem();
-        var offHand = Minecraft.getInstance().player.getOffhandItem();
+    public ChainCollisionEntityMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
 
-        if (mainHand.is(ZiplineItems.ZIPLINE) || offHand.is(ZiplineItems.ZIPLINE)) {
-            cir.setReturnValue(false);
+    @Override
+    public boolean canCollideWith(Entity entity) {
+        var defaultValue = super.canCollideWith(entity);
+
+        if (!defaultValue) {
+            return false;
         }
+
+        if (!(entity instanceof Player player)) {
+            return true;
+        }
+
+        var mainHand = player.getMainHandItem();
+        var offHand = player.getOffhandItem();
+
+        return !mainHand.is(ZiplineItems.ZIPLINE) && !offHand.is(ZiplineItems.ZIPLINE);
     }
 }
